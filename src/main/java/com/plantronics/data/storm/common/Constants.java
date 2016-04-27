975b9e32f5374e9c893ce99be9f5c24f76e13dc0
Ramakrishnan <mramakrishnan@sclmac-1002013.plt.plantronics.com>
2016-04-27 15:20:24 -0700

    adding pub nub constant and a new topology

--- src/main/java/com/plantronics/data/storm/common/Constants.java
+++ src/main/java/com/plantronics/data/storm/common/Constants.java
@@ -4,7 +4,7 @@ package com.plantronics.data.storm.common;
  * Created by mramakrishnan on 4/12/16.
  */
 public final class Constants {
-        public static String PUBNUB_PUB_KEY="";
-    public static String PUBNUB_SUB_KEY="";
-    public static String PUBNUB_SUB_CHANNEL="";
+    public static String PUBNUB_PUB_KEY="pub-c-8ef50afc-2aa5-44f5-b120-3bf66fec3002";
+    public static String PUBNUB_SUB_KEY="sub-c-6d89a260-c160-11e2-9a15-02ee2ddab7fe";
+    public static String PUBNUB_SUB_CHANNEL="SM01";
 }
