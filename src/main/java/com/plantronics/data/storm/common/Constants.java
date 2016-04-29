0543ef95f8414621f3c53250bde1a3e58bfe8b76
Ramakrishnan <formanojr@gmail.com>
2016-04-28 21:05:35 -0700

    changes constants for pub nub

--- src/main/java/com/plantronics/data/storm/common/Constants.java
+++ src/main/java/com/plantronics/data/storm/common/Constants.java
@@ -4,7 +4,7 @@ package com.plantronics.data.storm.common;
  * Created by mramakrishnan on 4/12/16.
  */
 public final class Constants {
-    public static String PUBNUB_PUB_KEY="pub-c-8ef50afc-2aa5-44f5-b120-3bf66fec3002";
-    public static String PUBNUB_SUB_KEY="sub-c-6d89a260-c160-11e2-9a15-02ee2ddab7fe";
-    public static String PUBNUB_SUB_CHANNEL="SM01";
+    public static String PUBNUB_PUB_KEY="pub-c-d1314104-910c-46c1-9e58-32d7504b9a01";
+    public static String PUBNUB_SUB_KEY="sub-c-cf5b662a-0da9-11e6-996b-0619f8945a4f";
+    public static String PUBNUB_SUB_CHANNEL="demo";
 }
